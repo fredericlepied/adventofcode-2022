068509db0601b7a2764dbfe440c94296314d9309
Frederic Lepied <flepied@redhat.com>
2022-12-11 08:11:07 +0100

    day 11 part 1

---
+++ day11/day11.go
@@ -0,0 +1,119 @@
+// https://adventofcode.com/2022/day/11
+
+package day11
+
+import (
+	"fmt"
+	//re "regexp"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type monkey struct {
+	items     []int
+	operation string
+	divisible int
+	valid     int
+	invalid   int
+	inspected int
+}
+
+// ByAge implements sort.Interface based on the Age field.
+type ByInspected []monkey
+
+func (a ByInspected) Len() int           { return len(a) }
+func (a ByInspected) Less(i, j int) bool { return a[i].inspected > a[j].inspected }
+func (a ByInspected) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+func ComputeResult(lines []string) (result int) {
+	fmt.Println("ComputeResult")
+
+	//number_regexp := re.MustCompile("[,-]")
+
+	var monkeys = []monkey{}
+	current := monkey{}
+
+	for idx := 0; idx < len(lines); idx++ {
+		line := lines[idx]
+		if strings.HasPrefix(line, "Monkey ") {
+			if current.divisible != 0 {
+				monkeys = append(monkeys, current)
+			}
+			new := monkey{}
+			current = new
+			current.divisible = idx
+		} else if strings.HasPrefix(line, "  Starting items: ") {
+			for _, item := range strings.Split(strings.TrimPrefix(line, "  Starting items: "), ", ") {
+				n, _ := strconv.Atoi(item)
+				current.items = append(current.items, n)
+			}
+		} else if strings.HasPrefix(line, "  Operation: new = ") {
+			current.operation = strings.TrimPrefix(line, "  Operation: new = ")
+		} else if strings.HasPrefix(line, "  Test: divisible by ") {
+			op, _ := strconv.Atoi(strings.TrimPrefix(line, "  Test: divisible by "))
+			current.divisible = op
+		} else if strings.HasPrefix(line, "    If true: throw to monkey ") {
+			op, _ := strconv.Atoi(strings.TrimPrefix(line, "    If true: throw to monkey "))
+			current.valid = op
+		} else if strings.HasPrefix(line, "    If false: throw to monkey ") {
+			op, _ := strconv.Atoi(strings.TrimPrefix(line, "    If false: throw to monkey "))
+			current.invalid = op
+		}
+		//res := order_regexp.FindStringSubmatch(line)
+	}
+	monkeys = append(monkeys, current)
+	fmt.Println(monkeys)
+
+	for round := 0; round < 20; round++ {
+		fmt.Println("round", round+1)
+		for id := range monkeys {
+			for _, item := range monkeys[id].items {
+				result := executeOperation(monkeys[id].operation, item)
+				monkeys[id].inspected += 1
+				var idx int
+				if result%monkeys[id].divisible == 0 {
+					idx = monkeys[id].valid
+				} else {
+					idx = monkeys[id].invalid
+				}
+				monkeys[idx].items = append(monkeys[idx].items, result)
+			}
+			monkeys[id].items = nil
+		}
+		for id := range monkeys {
+			fmt.Println(id, monkeys[id].items, monkeys[id].inspected)
+		}
+	}
+	sort.Sort(ByInspected(monkeys))
+	return monkeys[0].inspected * monkeys[1].inspected
+}
+
+func executeOperation(op string, val int) (result int) {
+	part := strings.Split(op, " ")
+	var x int
+	var y int
+
+	if part[0] == "old" {
+		x = val
+	} else {
+		r, _ := strconv.Atoi(part[0])
+		x = r
+	}
+
+	if part[2] == "old" {
+		y = val
+	} else {
+		r, _ := strconv.Atoi(part[2])
+		y = r
+	}
+
+	if part[1] == "*" {
+		result = x * y
+	}
+	if part[1] == "+" {
+		result = x + y
+	}
+	return int(math.Round(float64(result / 3)))
+}
---
+++ day11/day11_test.go
@@ -0,0 +1,75 @@
+package day11
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func check(input []string, want int, t *testing.T) {
+	got := ComputeResult(input)
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func TestComputeResult(t *testing.T) {
+	// part 1
+	check(shortList, 10605, t)
+
+	longList := readFile("input.txt")
+	check(longList, 58322, t)
+
+	// part 2
+}
+
+func readFile(fpath string) (lines []string) {
+	readFile, err := os.Open(fpath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		lines = append(lines, line)
+	}
+	readFile.Close()
+
+	return lines
+}
+
+var shortList = []string{
+	"Monkey 0:",
+	"  Starting items: 79, 98",
+	"  Operation: new = old * 19",
+	"  Test: divisible by 23",
+	"    If true: throw to monkey 2",
+	"    If false: throw to monkey 3",
+	"",
+	"Monkey 1:",
+	"  Starting items: 54, 65, 75, 74",
+	"  Operation: new = old + 6",
+	"  Test: divisible by 19",
+	"    If true: throw to monkey 2",
+	"    If false: throw to monkey 0",
+	"",
+	"Monkey 2:",
+	"  Starting items: 79, 60, 97",
+	"  Operation: new = old * old",
+	"  Test: divisible by 13",
+	"    If true: throw to monkey 1",
+	"    If false: throw to monkey 3",
+	"",
+	"Monkey 3:",
+	"  Starting items: 74",
+	"  Operation: new = old + 3",
+	"  Test: divisible by 17",
+	"    If true: throw to monkey 0",
+	"    If false: throw to monkey 1",
+}
