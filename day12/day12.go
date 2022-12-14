a365729e9a14352fdca71ee63e009925ccb777d9
Frederic Lepied <flepied@redhat.com>
2022-12-14 09:50:36 +0100

    day12 part1 corrected and part2

--- day12/day12.go
+++ day12/day12.go
@@ -4,98 +4,47 @@ package day12
 
 import (
 	"fmt"
+	"image"
+	"strings"
 )
 
-var possiblemoves = [][2]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-
-func visited(x int, y int, path [][2]int) bool {
-	for idx := range path {
-		if x == path[idx][0] && y == path[idx][1] {
-			return true
-		}
-	}
-	return false
-}
-
-func getHigh(x int, y int, lines []string) int {
-	val := lines[y][x]
-	if val == 'S' {
-		return 0
-	}
-	if val >= 'a' && val <= 'z' {
-		return int(val - 'a' + 1)
-	} else if val == 'E' {
-		return int('z' - 'a' + 2)
-	} else {
-		return 1000000
-	}
-}
-
-func getMoves(moves [][2]int, x int, y int, lines []string) [][2]int {
-	for idx := range possiblemoves {
-		newX := x + possiblemoves[idx][0]
-		newY := y + possiblemoves[idx][1]
-
-		// filter out invalid moves
-		if newY < 0 || newY >= len(lines) {
-			continue
-		}
-		if newX < 0 || newX >= len(lines[newY]) {
-			continue
-		}
-		// filter lower point
-		if getHigh(newX, newY, lines) > getHigh(x, y, lines)+1 {
-			continue
-		}
-		// filter out if already on the list
-		if visited(newX, newY, moves) {
-			continue
-		}
-		move := [2]int{newX, newY}
-		moves = append(moves, move)
-	}
-	return moves
-}
-
-func findTarget(lines []string) (x int, y int) {
-	for y = range lines {
-		for x = range lines[y] {
-			if lines[y][x] == 'E' {
-				return x, y
+func ComputeResult(input []byte) (int, int) {
+	fmt.Println("ComputeResult")
+	var start, end image.Point
+	height := map[image.Point]rune{}
+	for x, s := range strings.Fields(string(input)) {
+		for y, r := range s {
+			height[image.Point{x, y}] = r
+			if r == 'S' {
+				start = image.Point{x, y}
+			} else if r == 'E' {
+				end = image.Point{x, y}
 			}
 		}
 	}
-	return x, y
-}
+	height[start], height[end] = 'a', 'z'
+	dist := map[image.Point]int{end: 0}
+	queue := []image.Point{end}
+	var shortest *image.Point
 
-func ComputeResult(lines []string) int {
-	targetX, targetY := findTarget(lines)
-	fmt.Println("ComputeResult", targetX, targetY)
-	moves := getMoves(nil, 0, 0, lines)
-	length := 0
-	finished := false
-	for !finished {
-		futureMoves := [][2]int{}
-		length += 1
-		for i := range moves {
-			move := moves[i]
-			futureMoves = getMoves(futureMoves, move[0], move[1], lines)
-			finished = move[0] == targetX && move[1] == targetY
-			lines[move[1]] = lines[move[1]][:move[0]] + "+" + lines[move[1]][move[0]+1:]
-			if finished {
-				break
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+
+		if height[cur] == 'a' && shortest == nil {
+			shortest = &cur
+		}
+
+		for _, d := range []image.Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
+			next := cur.Add(d)
+			_, seen := dist[next]
+			_, valid := height[next]
+
+			if !seen && valid && height[cur] <= height[next]+1 {
+				dist[next] = dist[cur] + 1
+				queue = append(queue, next)
 			}
 		}
-		moves = futureMoves
-		if !finished && len(moves) == 0 {
-			fmt.Println("not found")
-			return -1
-		}
-		fmt.Print("\033[H\033[2J")
-		fmt.Println()
-		for i := range lines {
-			fmt.Println(lines[i])
-		}
 	}
-	return length
+	return dist[start], dist[*shortest]
 }
--- day12/day12_test.go
+++ day12/day12_test.go
@@ -1,58 +1,25 @@
 package day12
 
 import (
-	"bufio"
-	"fmt"
 	"os"
 	"testing"
 )
 
-func check(input []string, want int, t *testing.T) {
-	got := ComputeResult(input)
+func check(input []byte, want int, want2 int, t *testing.T) {
+	got, got2 := ComputeResult(input)
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
+	}
+	if got2 != want2 {
+		t.Errorf("part2: got %d, wanted %d", got2, want2)
 	}
 }
 
 func TestComputeResult(t *testing.T) {
 	// part 1
-	//check(veryShortList, 31, t)
-	check(shortList, 31, t)
-
-	longList := readFile("input.txt")
-	check(longList, 2, t)
-
-	// part 2
-}
-
-func readFile(fpath string) (lines []string) {
-	readFile, err := os.Open(fpath)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
-
-	fileScanner.Split(bufio.ScanLines)
-
-	for fileScanner.Scan() {
-		line := fileScanner.Text()
-		lines = append(lines, line)
-	}
-	readFile.Close()
-
-	return lines
-}
-
-var veryShortList = []string{
-	"aEbqponm",
-}
-
-var shortList = []string{
-	"Sabqponm",
-	"abcryxxl",
-	"accszExk",
-	"acctuvwj",
-	"abdefghi",
+	small, _ := os.ReadFile("small.txt")
+	input, _ := os.ReadFile("input.txt")
+	check(small, 31, 29, t)
+	check(input, 449, 443, t)
 }
