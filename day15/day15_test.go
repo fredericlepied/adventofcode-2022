fa04056c91308c5fa6d4458d2e62a3ad5135c7ab
Frederic Lepied <flepied@redhat.com>
2022-12-15 21:33:04 +0100

    day15 refactor test to use one check function

--- day15/day15_test.go
+++ day15/day15_test.go
@@ -5,18 +5,9 @@ import (
 	"testing"
 )
 
-func check(fname string, y int, want int, t *testing.T) {
+func check(fname string, y int, want int, t *testing.T, f func([]string, int) int) {
 	input := u.ReadFile(fname)
-	got := ComputeResult(input, y)
-
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
-	}
-}
-
-func check2(fname string, max int, want int, t *testing.T) {
-	input := u.ReadFile(fname)
-	got := ComputeResult2(input, max)
+	got := f(input, y)
 
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
@@ -25,12 +16,12 @@ func check2(fname string, max int, want int, t *testing.T) {
 
 func TestComputePart1(t *testing.T) {
 	// part 1
-	check("small.txt", 10, 26, t)
-	check("input.txt", 2000000, 4737443, t)
+	check("small.txt", 10, 26, t, ComputeResult)
+	check("input.txt", 2000000, 4737443, t, ComputeResult)
 }
 
 func TestComputePart2(t *testing.T) {
 	// part 2
-	check2("small.txt", 20, 56000011, t)
-	check2("input.txt", 4000000, 11482462818989, t)
+	check("small.txt", 20, 56000011, t, ComputeResult2)
+	check("input.txt", 4000000, 11482462818989, t, ComputeResult2)
 }
