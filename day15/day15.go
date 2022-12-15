bdc7927e800948a9c78a8f269161fc078727f5db
Frederic Lepied <flepied@redhat.com>
2022-12-15 12:53:52 +0100

    day15 part2

--- day15/day15.go
+++ day15/day15.go
@@ -5,10 +5,17 @@ package day15
 import (
 	"fmt"
 	re "regexp"
-	//"sort"
-	"image"
 	"strconv"
 )
+
+type Coord struct {
+	X int
+	Y int
+}
+
+func (self Coord) distance(coord Coord) int {
+	return abs(self.X-coord.X) + abs(self.Y-coord.Y)
+}
 
 func displayImg(img [][]byte) {
 	for idx := range img {
@@ -70,22 +77,27 @@ func set(x int, y int, val byte, img map[int]byte, targetY int) {
 	}
 }
 
-func ComputeResult(lines []string, targetY int) (result int) {
-	fmt.Println("ComputeResult")
-
+func initBeacons(lines []string, beacons map[Coord]Coord) {
 	line_regexp := re.MustCompile("Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)")
 
-	beacons := map[image.Point]image.Point{}
 	for idx := 0; idx < len(lines); idx++ {
 		line := lines[idx]
 		res := line_regexp.FindStringSubmatch(line)
-		beacons[image.Point{s2i(res[1]), s2i(res[2])}] = image.Point{s2i(res[3]), s2i(res[4])}
+		beacons[Coord{s2i(res[1]), s2i(res[2])}] = Coord{s2i(res[3]), s2i(res[4])}
 	}
+}
+
+func ComputeResult(lines []string, targetY int) (result int) {
+	fmt.Println("ComputeResult")
+
+	beacons := map[Coord]Coord{}
+	initBeacons(lines, beacons)
+
 	img := map[int]byte{}
 	for sensor, beacon := range beacons {
 		set(sensor.X, sensor.Y, 'S', img, targetY)
 		set(beacon.X, beacon.Y, 'B', img, targetY)
-		dist := abs(sensor.X-beacon.X) + abs(sensor.Y-beacon.Y)
+		dist := sensor.distance(beacon)
 		if targetY >= sensor.Y-dist && targetY <= sensor.Y+dist {
 			for x := sensor.X - dist; x < sensor.X+dist; x++ {
 				dist2 := abs(x-sensor.X) + abs(targetY-sensor.Y)
@@ -101,4 +113,87 @@ func ComputeResult(lines []string, targetY int) (result int) {
 		}
 	}
 	return result
+}
+
+func initBeacons2(lines []string, beacons map[Coord]Coord, distances map[Coord]int) {
+	line_regexp := re.MustCompile("Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)")
+
+	for idx := 0; idx < len(lines); idx++ {
+		line := lines[idx]
+		res := line_regexp.FindStringSubmatch(line)
+		sensor := Coord{s2i(res[1]), s2i(res[2])}
+		beacon := Coord{s2i(res[3]), s2i(res[4])}
+		dist := sensor.distance(beacon)
+		beacons[sensor] = beacon
+		distances[sensor] = dist
+	}
+}
+
+func canContainUnseenPoint(
+	sensor Coord,
+	distances map[Coord]int,
+	minVal Coord,
+	maxVal Coord) bool {
+	corners := []Coord{
+		{X: minVal.X, Y: minVal.Y},
+		{X: minVal.X, Y: maxVal.Y},
+		{X: maxVal.X, Y: minVal.Y},
+		{X: maxVal.X, Y: maxVal.Y},
+	}
+	for _, corner := range corners {
+		distance := sensor.distance(corner)
+		if distance > distances[sensor] {
+			return true
+		}
+	}
+	return false
+}
+
+func findUnseenPoint(
+	beacons map[Coord]Coord,
+	distances map[Coord]int,
+	minVal Coord,
+	maxVal Coord) Coord {
+	if minVal == maxVal {
+		return minVal
+	}
+	mid := Coord{(minVal.X + maxVal.X) / 2, (minVal.Y + maxVal.Y) / 2}
+
+	quadrants := make([][]Coord, 4)
+	quadrants[0] = []Coord{minVal, mid}
+	quadrants[1] = []Coord{Coord{X: mid.X + 1, Y: minVal.Y}, Coord{X: maxVal.X, Y: mid.Y}}
+	quadrants[2] = []Coord{Coord{X: minVal.X, Y: mid.Y + 1}, Coord{X: mid.X, Y: maxVal.Y}}
+	quadrants[3] = []Coord{Coord{X: mid.X + 1, Y: mid.Y + 1}, maxVal}
+
+	for _, quadrant := range quadrants {
+		if quadrant[0].X > quadrant[1].X || quadrant[0].Y > quadrant[1].Y {
+			continue
+		}
+
+		allPairsCanContain := true
+		for sensor := range beacons {
+			if !canContainUnseenPoint(sensor, distances, quadrant[0], quadrant[1]) {
+				allPairsCanContain = false
+				break
+			}
+		}
+		if allPairsCanContain {
+			k := findUnseenPoint(beacons, distances, quadrant[0], quadrant[1])
+			if k.X != -1 || k.Y != -1 {
+				return k
+			}
+		}
+	}
+	return Coord{X: -1, Y: -1}
+}
+
+func ComputeResult2(lines []string, maxN int) (result int) {
+	fmt.Println("ComputeResult2")
+
+	beacons := map[Coord]Coord{}
+	distances := map[Coord]int{}
+	initBeacons2(lines, beacons, distances)
+
+	point := findUnseenPoint(beacons, distances, Coord{0, 0}, Coord{maxN, maxN})
+	return point.X*4000000 + point.Y
 }
--- day15/day15_test.go
+++ day15/day15_test.go
@@ -16,10 +16,25 @@ func check(fname string, y int, want int, t *testing.T) {
 	}
 }
 
-func TestComputeResult(t *testing.T) {
+func check2(fname string, max int, want int, t *testing.T) {
+	input := readFile(fname)
+	got := ComputeResult2(input, max)
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func TestComputePart1(t *testing.T) {
 	// part 1
 	check("small.txt", 10, 26, t)
 	check("input.txt", 2000000, 4737443, t)
+}
+
+func TestComputePart2(t *testing.T) {
+	// part 2
+	check2("small.txt", 20, 56000011, t)
+	check2("input.txt", 4000000, 11482462818989, t)
 }
 
 func readFile(fpath string) (lines []string) {
